af11bd4d1e41c5941662e5f64b9406eb72f171cb
Antti <anttkem@metropolia.fi>
2022-03-25 03:29:53 +0200

    Forms

---
+++ labs/11 Unit Testing/src/notes.js
@@ -0,0 +1,23 @@
+var notes = (function() {
+    var list = [];
+
+    return {
+        add: function(note) {
+            if (note) {
+                var item = {timestamp: Date.now(), text: note};
+                list.push(item);
+                return true;
+            }
+            return false;
+        },
+        remove: function(index) {},
+        count: function() {
+            return list.length;
+        },
+        list: function() {},
+        find: function(str) {},
+        clear: function() {
+            list.splice(0, list.length);
+        }
+    }
+}());
