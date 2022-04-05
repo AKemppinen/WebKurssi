594b3daa91daaf59f66eece2747704ebdf6777c7
Antti <anttkem@metropolia.fi>
2022-04-05 09:09:58 +0300

    10 ja 11

--- labs/11 Unit Testing/spec/notes.spec.js
+++ labs/11 Unit Testing/spec/notes.spec.js
@@ -7,23 +7,64 @@ describe('notes module', function () {
         notes.add('fourth note');
         notes.add('fifth note');
     });
-    it("should be able to add a new note", function () {
-        expect(notes.add('sixth note')).toBe(true);
-        expect(notes.count()).toBe(6);
-    });
-    it("should ignore blank notes", function () {
-        expect(notes.add('')).toBe(false);
-        expect(notes.count()).toBe(5);
-    });
-    it('should ignore notes containing only whitespace', function() {
-        expect(notes.add('   ')).toBe(false);
-        expect(notes.count()).toBe(5);
-        pending();
+
+    describe('note tests', function() {
+
+        it("should be able to add a new note", function () {
+            expect(notes.add('sixth note')).toBe(true);
+            expect(notes.count()).toBe(6);
+        });
+        it("should ignore blank notes", function () {
+            expect(notes.add('')).toBe(false);
+            expect(notes.count()).toBe(5);
+        });
+        it('should ignore notes containing only whitespace', function() {
+            expect(notes.add('   ')).toBe(false);
+            expect(notes.count()).toBe(5);
+
+        });
+
+        it('should require a string parameter', function() {
+            expect(notes.add()).toBe(false);
+            expect(notes.count()).toBe(5);
+
+        });
     });
 
-    it('should require a string parameter', function() {
-        expect(notes.add()).toBe(false);
-        expect(notes.count()).toBe(5);
-        pending();
+
+    describe('finding a note', function() {
+
+        it('should find nothing', function() {
+            expect(notes.find()).toEqual(undefined);
+        });
+
+        it('should find first note', function() {
+            expect(notes.find('first note')).toEqual(notes.list()[0]);
+        });
     });
+
+    describe('returning list', function() {
+
+        it('should return list', function() {
+            expect(Array.isArray(notes.list())).toEqual(true);
+        });
+
+    });
+
+    describe('deleting a note', function() {
+
+        it('should remove nothing', function() {
+            expect(notes.remove()).toEqual(false);
+        });
+
+        it('should remove second note', function() {
+            expect(notes.remove(1)).toEqual(true);
+        });
+
+        it('should clear list', function() {
+            expect(notes.clear()).toEqual(0);
+        });
+    });
+
 });
+
--- labs/11 Unit Testing/src/notes.js
+++ labs/11 Unit Testing/src/notes.js
@@ -4,6 +4,9 @@ var notes = (function() {
     return {
         add: function(note) {
             if (note) {
+                note = note.trim();
+                if(note == "") return false;
+
                 var item = {timestamp: Date.now(), text: note};
                 list.push(item);
                 return true;
@@ -11,17 +14,29 @@ var notes = (function() {
             return false;
         },
         remove: function(index) {
-            list.splice(index, 1);
+            if(index) {
+                if (index > list.length - 1) return false;
+                return list.splice(index, 1).length > 0;
+
+            }
+            return false;
+
         },
         count: function() {
             return list.length;
         },
-        list: function() {},
+        list: function() {
+            return list;
+        },
         find: function(str) {
-            list.find(str);
+            if (str) {
+                return list.find(x => x.text === str);
+            }
+
         },
         clear: function() {
             list.splice(0, list.length);
+            return list.length;
         }
     }
 }());
